1a2afee1951b91d004b8666c80ff9e9eea846528
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2025-02-05 04:29:57 +0600

    Add files via upload

---
+++ Lab problem/Practice problem set 01/problem 08.sql
@@ -0,0 +1,4 @@
+-- 8. Output instructor names and buildings for each building an instructor has taught in. 
+-- Include instructor names who have not taught any classes (the building name should be NULL in this case).
+(Same as Query 6)
+
