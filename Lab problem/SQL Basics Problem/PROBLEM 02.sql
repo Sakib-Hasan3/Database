9bc624c50eef1ab04961eeb2a7eb1d99b752a9df
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2025-02-05 02:15:58 +0600

    Add files via upload

---
+++ Lab problem/SQL Basics Problem/PROBLEM 02.sql
@@ -0,0 +1,3 @@
+-- 2. Find out the ID and salary of the instructor who gets more than $85,000.
+SELECT ID, salary FROM instructor WHERE salary > 85000;
+
---
+++ Lab problem/SQL Basics Problem/PROBLEM 03.sql
@@ -0,0 +1,4 @@
+-- 4. List out the names of the instructors from Computer Science who have more than $70,000.
+SELECT name FROM instructor WHERE dept_name = 'Computer Science' AND salary > 70000;
+
+
