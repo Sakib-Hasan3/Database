2e0f87d0865c9ab9d2eb3f766773f1352081aff9
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2025-02-05 04:29:42 +0600

    Delete Lab problem/Practice problem set 01/problem 09.sql

--- Lab problem/Practice problem set 01/problem 09.sql
+++
@@ -1,8 +0,0 @@
--- 9. For each student who has retaken a course at least twice (i.e., the student has taken the course at least three times), 
--- show the course ID and the student's ID. Please display results in order of course ID and do not display duplicate rows.
-SELECT DISTINCT t.ID, t.course_id
-FROM takes t
-GROUP BY t.ID, t.course_id
-HAVING COUNT(*) >= 3
-ORDER BY t.course_id;
-
