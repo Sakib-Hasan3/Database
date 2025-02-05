b2f7570adf8fbc5b7a83fb0778aa0a9e59c74d17
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2025-02-05 15:52:40 +0600

    Add files via upload

---
+++ Intermidiate sql/Chapter 4/Dangerous in natural join.sql
@@ -0,0 +1,20 @@
+#Dangerous in Natural Joins
+
+
+1. Unintended Attribute Matching
+2. Loss of Information (Column Dropping)
+3. Ambiguity and Unpredictability
+4. Performance Issues
+5. Difficult Debugging
+
+
+Corrected version :
+SQL> 
+select name, title
+from student natural join takes, course
+where takes.course_id= course.course_id;
+
+Incorrect version :
+SQL>
+select name, title
+from student natural join takes natural join course;
